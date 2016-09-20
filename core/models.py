d5aeca9e521ad2e3e4fa729169216ef00e029888
Dean Armada <deanarmada@gmail.com>
2016-09-20 15:38:33 +0800

    Fixed account models

--- core/models.py
+++ core/models.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 
-from django.contrib.auth.models import User
 from django.db import models
 
 from core.methods import school_logo_upload_path_handler
@@ -34,27 +33,3 @@ class School(models.Model):
 
 	def __unicode__(self):
 		return self.name
-
-class UserType(models.Model):
-	name = models.CharField(max_length=50)
-
-	def __unicode__(self):
-		return self.name
-
-class UserAccount(models.Model):
-	user = models.ForeignKey(User)
-	user_type = models.ForeignKey(UserType)
-	school = models.ForeignKey(School)
-	middle_name = models.CharField(max_length=50)
-	street = models.TextField()
-	suburb = models.CharField(max_length=75)
-	state = models.ForeignKey(AUState)
-	post_code = models.IntegerField()
-	last_login = models.DateTimeField(auto_now_add=True)
-	date_added = models.DateTimeField(auto_now_add=True)
-
-	def get_name(self):
-		return "%s %s %s" % (self.user.first_name, self.user.middle_name, self.user.last_name)
-
-	def __unicode__(self):
-		return self.get_name()
